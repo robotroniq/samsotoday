6795f96a984f77b66955390ad012551ab8e56eaf
Sam So <robotronic0694@gmail.com>
2021-03-05 00:11:11 +0900

    initial version

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './samsotoday_logo.png';
 import './App.css';
 
 function App() {
@@ -7,16 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <span className="p1">#samso_today</span>
+          <br></br>
+          <span className="p2">UNDER CONSTRUCTION</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
